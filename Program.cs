49c7fafef52239e3476930c2b8595998919589cc
wbogatyrewa <veronika01-01@mail.ru>
2020-09-07 21:12:35 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace labs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWhat is your name? ");
+            var name = Console.ReadLine();
+            Console.WriteLine($"\nHello, {name}!");
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
