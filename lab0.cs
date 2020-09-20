0f60fd6f29d3e42b10587c715705dcbfb0e44831
wbogatyrewa <veronika01-01@mail.ru>
2020-09-20 21:28:55 +0500

    commit lab0

---
+++ lab0.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace labs
+{
+    class lab0
+    {
+        public void Main0()
+        {
+            Console.WriteLine("\nWhat is your name? ");
+            var name = Console.ReadLine();
+            Console.WriteLine($"\nHello, {name}!");
+        }
+    }
+}
